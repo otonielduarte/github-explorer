c1e5740312e65c1b716d6145026e1d7a3157511d
Otoniel Duarte <otonielduarte2@gmail.com>
2022-01-28 08:50:08 -0300

    fix url

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -4,7 +4,7 @@ export default function ({ repository }) {
       <li>
         <strong>{repository.name}</strong>
         <p>{repository.description}</p>
-        <a href="">{repository.url}</a>
+        <a href={repository.html_url}>{repository.html_url}</a>
       </li>
     </>
   )
