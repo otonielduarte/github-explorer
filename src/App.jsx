6325922a4d08d1a3a15e8755c9c86408fe1b7e8a
Otoniel Duarte <otonielduarte2@gmail.com>
2022-01-28 05:58:39 -0300

    basic configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import './styles/global.scss'
 
-function App() {
-    return <h1>Hello world</h1>
+export function App() {
+    return <h1>Hello world!</h1>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,4 @@
+import { render } from 'react-dom';
 import { App } from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById('root'))
